46de01b02f06f18a330b8df55e32d07ae42e0c08
maximilien Pater <70021238+maximilienalgo@users.noreply.github.com>
2021-01-08 08:42:58 +0100

    Delete DateNaissance.py

--- DateNaissance.py
+++
@@ -1,9 +0,0 @@
-class DateNaissance:
-
-    def __init__(self, jour, mois, annee):
-        self.jour = jour
-        self.mois = mois
-        self.annee = annee
-
-    def __str__(self):
-        return ("{} / {} / {}").format(self.jour, self.mois, self.annee)
